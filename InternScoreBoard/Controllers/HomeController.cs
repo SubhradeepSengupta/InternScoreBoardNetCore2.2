384e2eb411b336eeeb597adc53eb1dc008b763c0
Subhradeep Sengupta <subhradeep@promactinfo.com>
2020-01-16 16:33:30 +0530

    Basic functionality created

---
+++ InternScoreBoard/Controllers/HomeController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using InternScoreBoard.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace InternScoreBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UploadData(List<IFormFile> uploadedFiles, string submit)
+        {
+            if (ModelState.IsValid)
+            {
+                if (uploadedFiles.Count > 0)
+                {
+                    List<InternMarksDataModel> internMarksDatas = new List<InternMarksDataModel>();
+                    List<InternDetails> internDetails = new List<InternDetails>();
+
+                    foreach (var file in uploadedFiles)
+                    {
+                        if (file.FileName.EndsWith(".csv"))
+                        {
+                            //using var reader = new StreamReader(file.OpenReadStream());
+                            using(StreamReader reader = new StreamReader(file.OpenReadStream()))
+                            {
+                                var lines = reader.ReadToEnd();
+
+                                if (!lines.Equals(""))
+                                {
+                                    var singleLine = lines.Split("\r\n");
+
+                                    switch (submit)
+                                    {
+                                        case "all":
+                                            for (int i = 1; i < singleLine.Length; i++)
+                                            {
+                                                if (internDetails.Any(intern => intern.InternName.Equals(singleLine[i].Split(',').First())))
+                                                {
+                                                    internDetails.Where(intern => intern.InternName.Equals(singleLine[i].Split(',').First())).First().Courses.Add(new CourseMarks
+                                                    {
+                                                        CourseName = Path.GetFileNameWithoutExtension(file.FileName),
+                                                        Percentage = singleLine[i].Split(',').Last()
+                                                    });
+                                                }
+                                                else
+                                                {
+                                                    InternDetails intern = new InternDetails
+                                                    {
+                                                        InternName = singleLine[i].Split(',').First(),
+                                                        Email = singleLine[i].Split(',').Skip(1).First(),
+                                                        Courses = new List<CourseMarks>
+                                            {
+                                                new CourseMarks
+                                                {
+                                                    CourseName = Path.GetFileNameWithoutExtension(file.FileName),
+                                                    Percentage = singleLine[i].Split(',').Last()
+                                                }
+                                            }
+                                                    };
+                                                    internDetails.Add(intern);
+                                                }
+                                            }
+                                            break;
+
+                                        case "individual":
+                                            InternMarksDataModel internMarks = new InternMarksDataModel()
+                                            {
+                                                CourseName = Path.GetFileNameWithoutExtension(file.FileName),
+                                                ColumnNames = singleLine[0].Split(',').ToList()
+                                            };
+
+                                            internMarks.InternNameAndScores = new List<List<string>>();
+
+                                            for (int i = 1; i < singleLine.Length; i++)
+                                            {
+                                                internMarks.InternNameAndScores.Add(singleLine[i].Split(',').ToList());
+                                            }
+                                            internMarksDatas.Add(internMarks);
+                                            break;
+                                    }
+                                }
+                                else
+                                    ModelState.AddModelError(string.Empty, "No data found");
+
+                                reader.Close();
+                            }
+                        }
+                        else
+                            ModelState.AddModelError(string.Empty, "Wrong file format.");
+                    }
+
+                    foreach (var detail in internDetails)
+                    {
+                        foreach (var score in detail.Courses)
+                        {
+                            detail.AllOverScore += double.Parse(score.Percentage.Remove(score.Percentage.Length - 1, 1));
+                        }
+                        detail.AllOverScore /= detail.Courses.Count;
+                    }
+
+                    if (internDetails.Count > 0)
+                    {
+                        ViewData["CourseColumnCount"] = (uploadedFiles.Count) * 2;
+                        return View("index", internDetails.OrderByDescending(c => c.AllOverScore));
+                    }
+                    else if (internMarksDatas.Count > 0)
+                    {
+                        return View("IndividualReport", internMarksDatas);
+                    }
+                }
+                else
+                    ModelState.AddModelError(string.Empty, "No file(s) chosen.");
+            }
+            return View("index");
+        }
+
+        [HttpGet]
+        public IActionResult IndividualReport()
+        {
+            return View();
+        }
+    }
+}
---
+++ InternScoreBoard/Models/InternMarksDataModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternScoreBoard.Models
+{
+    public class InternMarksDataModel
+    {
+        public string CourseName { get; set; }
+        public List<string> ColumnNames { get; set; }
+        public List<List<string>> InternNameAndScores { get; set; }
+    }
+
+    public class InternDetails
+    {
+        public string InternName { get; set; }
+        public string Email { get; set; }
+        public List<CourseMarks> Courses { get; set; }
+        public double AllOverScore { get; set; }
+    }
+
+    public class CourseMarks
+    {
+        public string CourseName { get; set; }
+        public string Percentage { get; set; }
+    }
+}
